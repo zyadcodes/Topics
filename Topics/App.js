6533e1ea85c3e3b2d1fe700ef971f6ef8fc84ee3
zyadelgohary <zyadgohary@hotmail.com>
2021-02-19 21:24:21 -0800

    Added firebase successfully

--- Topics/App.js
+++ Topics/App.js
@@ -1,16 +1,36 @@
 // This is going to be the main file that is run when the app is first launched
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useRef} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import MainStackNavigator from './src/screens/MainStackNavigator'
+import MainStackNavigator from './src/screens/MainStackNavigator';
+import analytics from '@react-native-firebase/analytics';
 
 // Creates the functional component
 const App = (props) => {
+  // Creates references for analytics
+  const navigationRef = useRef();
+  const routeNameRef = useRef();
+
   // Renders the component
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer
+        ref={navigationRef}
+        onReady={() =>
+          (routeNameRef.current = navigationRef.current.getCurrentRoute().name)
+        }
+        onStateChange={async () => {
+          const previousRouteName = routeNameRef.current;
+          const currentRouteName = getActiveRouteName(state);
+
+          if (previousRouteName !== currentRouteName) {
+            await analytics().logScreenView({
+              screen_name: currentRouteName,
+              screen_class: currentRouteName,
+            });
+          }
+        }}>
         <MainStackNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
